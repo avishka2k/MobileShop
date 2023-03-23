79f6440a2459b4dcdf860a5058755a7f94ae9ccd
avishka2k <avishka2k@gmail.com>
2023-03-23 09:41:06 +0530

    product add feature

---
+++ MobileShop/Data/ProductDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MobileShop.Models;
+
+namespace MobileShop.Data
+{
+    public class ProductDbContext: DbContext
+    {
+        public ProductDbContext()
+        {
+
+        }
+        public ProductDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<ProductModel> Products { get; set; }
+        public DbSet<TagsModel> Tags { get; set; }
+    }
+}
---
+++ MobileShop/Migrations/20230322184005_Initial Migration - product.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MobileShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigrationproduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PictureUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BrandName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Colors = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Reviews = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReviewScore = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Stock = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Delivery = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SKU = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tags = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TagName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TagsModelId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tags_Tags_TagsModelId",
+                        column: x => x.TagsModelId,
+                        principalTable: "Tags",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_TagsModelId",
+                table: "Tags",
+                column: "TagsModelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
