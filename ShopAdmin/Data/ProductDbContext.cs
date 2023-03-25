97940af6edcadecc7f68565ee28de40b23c77c3c
kalanikasthuriarachchi <kalanikasthuriarachchi98@gmail.com>
2023-03-25 23:37:34 +0530

    update admin dashboard

---
+++ ShopAdmin/Data/ProductDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopAdmin.Models;
+
+namespace ShopAdmin.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext()
+        {
+
+        }
+        public ProductDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CategoryModel> Categories { get; set; }
+    }
+
+}
