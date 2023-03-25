e495df0dca7ea7038ec66e1fb36ca614f88b7b88
isuru senarathne <IsuruUSenarathne>
2023-03-25 21:35:52 +0530

    shop admin update

---
+++ ShopAdmin/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace ShopAdmin.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+        public string BrandName { get; set; }
+        public string Colors { get; set; }
+        public string Reviews { get; set; }
+        public string ReviewScore { get; set; }
+        public string Price { get; set; }
+        public string Stock { get; set; }
+        public string Delivery { get; set; }
+        public string SKU { get; set; }
+    }
+}
