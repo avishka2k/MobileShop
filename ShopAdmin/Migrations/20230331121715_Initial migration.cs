006332bdc1b98020ee732813d82c320680c7053f
IsuruUSenarathne <isurusenarathne9@gmail.com>
2023-03-31 21:57:39 +0530

    multiple image upload

--- ShopAdmin/Migrations/20230331121715_Initial migration.cs
+++ ShopAdmin/Migrations/20230331121715_Initial migration.cs
@@ -33,7 +33,6 @@ namespace ShopAdmin.Migrations
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    PictureUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     BrandName = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Colors = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Reviews = table.Column<string>(type: "nvarchar(max)", nullable: false),
@@ -47,6 +46,32 @@ namespace ShopAdmin.Migrations
                 {
                     table.PrimaryKey("PK_Products", x => x.Id);
                 });
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Images_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_ProductId",
+                table: "Images",
+                column: "ProductId");
         }
 
         /// <inheritdoc />
@@ -54,6 +79,9 @@ namespace ShopAdmin.Migrations
         {
             migrationBuilder.DropTable(
                 name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Images");
 
             migrationBuilder.DropTable(
                 name: "Products");
