d4cd94c4cd1e915b98e0fec022f7de233248f48f
TharushiVidanapathirana <malkividunima20050730@gmail.com>
2023-04-23 23:11:23 +0530

    update controller

---
+++ ShopAdmin/Controllers/NotificationsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopAdmin.Data;
+using ShopAdmin.Models;
+
+namespace ShopAdmin.Controllers
+{
+    
+    public class NotificationsController : Controller
+    {
+        private readonly ProductDbContext _dbContext;
+        public NotificationsController(ProductDbContext context)
+        {
+            _dbContext = context;
+        }
+        public IActionResult Index()
+        {
+            var homeView = new Notifications
+            {
+                Orders = _dbContext.Orders.ToList(),
+            };
+            return View(homeView);
+        }
+    }
+}
--- ShopAdmin/Controllers/OrderController.cs
+++ ShopAdmin/Controllers/OrderController.cs
@@ -20,7 +20,11 @@ namespace ShopAdmin.Controllers
 
         public async Task<IActionResult> Details(int? id)
         {
-            var order = await _dbContext.Orders.Include(p => p.Carts).ThenInclude(c => c.Product).FirstOrDefaultAsync(o => o.Id == id);
+            //var order = await _dbContext.Orders.Include(p => p.Carts).ThenInclude(c => c.Product).ThenInclude(f => f.Category).FirstOrDefaultAsync(o => o.Id == id);
+            var order = await _dbContext.Orders
+                .Include("Carts.Product.Category")
+                .Include("Carts.Product.Brand")
+                .FirstOrDefaultAsync(o => o.Id == id);
             return View(order);
         }
     }
