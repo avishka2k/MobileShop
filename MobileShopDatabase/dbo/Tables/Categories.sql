e8fc2f7a56cab93d73c12b2b63fbf309aef56b62
avishka2k <avishka2k@gmail.com>
2023-06-17 15:35:30 +0530

    shop database

---
+++ MobileShopDatabase/dbo/Tables/Categories.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Categories] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [Name]       NVARCHAR (MAX) NOT NULL,
+    [PictureUrl] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
